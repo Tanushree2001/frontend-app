5fd76b8c1c60a2d85e6738c5f33b383ceed860b2
tanushree gupta <tanushree.gupta@innoraft.com>
2023-10-16 19:31:26 +0530

    after break

--- web/themes/custom/simplesmart/js/myslider.js
+++ web/themes/custom/simplesmart/js/myslider.js
@@ -1,42 +1,25 @@
-console.log("its tanu here");
-
-// function ($, Drupal) {
-//   Drupal.behaviors.mySlickSlider = {
-//     attach: function (context, settings) {
-//       // Initialize Slick Slider on the wrapper element.
-//       console.log("Hello");
-//       $('.view-display-id-block_1 .view-content').slick({
-//         dots: true,
-//         infinite: false,
-//         speed: 300,
-//         // autoplay: true,
-//         // autoplaySpeed: 2000,
-//         slidesToShow: 1,
-//         adaptiveHeight: true,
-//         prevArrow: '<span class="Slick-Prev arrow"><i class="lni lni-chevron-left"></i></span>',
-//         nextArrow: '<span class="Slick-Next arrow"><i class="lni lni-chevron-right"></i></span>',
-//       });
-//     }
-//   };
-// }) (jQuery, Drupal);
-
-console.log("Tanu here");
 (function ($, Drupal) {
   Drupal.behaviors.mySlickSlider = {
     attach: function (context, settings) {
-      console.log("Hi");
-      console.log("Hiiiiiiiiii");
       // Initialize Slick Slider on the wrapper element.
       $('.view-display-id-block_1 .view-content').slick({
         dots: true,
         infinite: false,
         speed: 300,
-        autoplay: true,
+        autoplay: false,
         autoplaySpeed: 2000,
         slidesToShow: 1,
         adaptiveHeight: true,
+        prevArrow:
+          '<span class="Slick-Prev arrow"><i class="fa-solid fa-chevron-left"></i></span>',
+        nextArrow:
+          '<span class="Slick-Next arrow"><i class="fa-solid fa-chevron-right"></i></span>',
+      }),
+
+      // Check if field_code or field_codename_team2 contains 'IND'
+      $('.match_content--title:contains("IND")').each(function() {
+        $(this).closest('.inside_container').find('.go_india').css('display', 'block');
       });
-      console.log("Ho gya");
     }
   };
 })(jQuery, Drupal);
